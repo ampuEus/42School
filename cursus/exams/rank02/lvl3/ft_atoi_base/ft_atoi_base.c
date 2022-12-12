209ba413a5bb0160aa5432d6b6a3ff095b550f81
ampuEus <ampueus@gmail.com>
2022-12-13 00:11:30 +0100

    [Exam_rank02] ft_atoi_base done

--- cursus/exams/rank02/lvl3/ft_atoi_base/ft_atoi_base.c
+++ cursus/exams/rank02/lvl3/ft_atoi_base/ft_atoi_base.c
@@ -1,15 +1,47 @@
-#define BASE "0123456789abcdef"
+char	to_lower(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
+
+int	get_nbr(char c, int str_base)
+{
+	int nbr;
+
+	if (str_base <= 10)
+		nbr = str_base + '0';
+	else
+		nbr = str_base - 10 + 'a';
+
+	if (c >= '0' && c <= '9' && c <= nbr)
+		return (c - '0');
+	else if (c >= 'a' && c <= 'f' && c <= nbr)
+		return (c + 10 - 'a');
+	else
+		return (-1);
+}
 
 int	ft_atoi_base(const char *str, int str_base)
 {
+	char			sign = 1;
+	int				nbr;
+	int				output = 0;
 	unsigned int	i = 0;
-	unsigned int	len = 0;
-	char			sign = 0;
 
 	if (str[i] == '-')
-		sign = 1;
-
-	while(str[len])
-		len++;
-
+	{
+		sign = -1;
+		i++;
+	}
+	/*
+	while (str[i] == ' ')
+		i++;
+	*/
+	while ((nbr = get_nbr(to_lower(str[i]), str_base)) >= 0)
+	{
+		output = output * str_base + nbr;
+		i++;
+	}
+	return (output * sign);
 }
