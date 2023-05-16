fcced909bbda88659bb9c3e7aa47d3f93f982e7a
ampuEus <ampueus@gmail.com>
2023-05-16 22:27:06 +0200

    [libft] New funtion added - ft_doublefree

---
+++ cursus/lvl0/Libft/src/extended/ft_doublefree.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_doublefree.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daampuru <daampuru@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/14 00:04:06 by daampuru          #+#    #+#             */
+/*   Updated: 2023/05/14 00:13:08 by daampuru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../libft.h"
+
+char	ft_doublefree(char **table)
+{
+	unsigned int	line;
+
+	line = 0;
+	while(table[line])
+		free(table[line++]);
+	free(table);
+	return (0);
+}
