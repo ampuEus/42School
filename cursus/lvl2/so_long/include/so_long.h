3e7102243a185430ce6f1fa1930b95a70bd5f8ae
ampuEus <ampueus@gmail.com>
2023-06-04 17:36:22 +0200

    [so_long] Render img on window done

--- cursus/lvl2/so_long/include/so_long.h
+++ cursus/lvl2/so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: daampuru <daampuru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 17:43:48 by daampuru          #+#    #+#             */
-/*   Updated: 2023/06/04 16:41:04 by daampuru         ###   ########.fr       */
+/*   Updated: 2023/06/04 17:14:15 by daampuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@ typedef struct	s_gui {
 }			t_gui;
 
 /* ---------- Constants ---------- */
+//keycodes
+# define KEY_ESC 53
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+# define KEY_DOWN 125
+# define KEY_UP 126
 
 /* ---------- Functions ---------- */
 
--- cursus/lvl2/so_long/src/main.c
+++ cursus/lvl2/so_long/src/main.c
@@ -6,11 +6,12 @@
 /*   By: daampuru <daampuru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 17:45:07 by daampuru          #+#    #+#             */
-/*   Updated: 2023/06/04 17:02:35 by daampuru         ###   ########.fr       */
+/*   Updated: 2023/06/04 17:35:24 by daampuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include <stdio.h>
 
 /* This function need and "exit ()" intead of a "return ()", because if not
 when it return to the loop will make a segmentation fault" */
@@ -21,28 +22,45 @@ char	close_gui(t_gui	*gui)
 	exit (0);
 }
 
-/* Some keycodes:
-	ESC = 53
-	LEFT = 123
-	RIGHT = 124
-	DOWN = 125
-	UP = 126 */
 int	key_hook(int keycode, t_gui *gui)
 {
-	ft_printf("key = %i\n", keycode);
-	if (keycode == 53)
+	if (keycode == KEY_ESC)
 		close_gui(gui);
+	else if (keycode == KEY_LEFT)
+		ft_printf("Left = %i\n", keycode);
+	else if (keycode == KEY_RIGHT)
+		ft_printf("RIGHT = %i\n", keycode);
+	else if (keycode == KEY_DOWN)
+		ft_printf("DOWN = %i\n", keycode);
+	else if (keycode == KEY_UP)
+		ft_printf("UP = %i\n", keycode);
+	else
+		ft_printf("No Hooked key = %i\n", keycode);
 	return (keycode);
+}
+
+char	start_gui(t_gui *gui)
+{
+	char	*relative_path = "./assets/test.xpm";
+	int		img_width = 100;
+	int		img_height = 100;
+	void	*img;
+
+	gui->mlx = mlx_init();
+	gui->win = mlx_new_window(gui->mlx, 1920, 1080, "so_long");
+	img = mlx_xpm_file_to_image(gui->mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(gui->mlx, gui->win, img, 10, 10);
+	mlx_key_hook(gui->win, key_hook, gui);
+	mlx_hook(gui->win, 17, 1L<<0, close_gui, gui);
+	mlx_loop(gui->mlx);
+	return (0);
 }
 
 int	main (int argc, char *argv[])
 {
 	t_gui	gui;
 
-	gui.mlx = mlx_init();
-	gui.win = mlx_new_window(gui.mlx, 1920, 1080, "so_long");
-	mlx_key_hook(gui.win, key_hook, &gui);
-	mlx_hook(gui.win, 17, 1L<<0, close_gui, &gui);
-	mlx_loop(gui.mlx);
+	start_gui(&gui);
+
 	return (0);
 }
