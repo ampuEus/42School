0b9895efb289cc1af88a0f66d3a34ea28c896fe0
ampuEus <ampueus@gmail.com>
2023-06-04 18:09:02 +0200

    [so_long] img moves with arrows keys

--- cursus/lvl2/so_long/include/so_long.h
+++ cursus/lvl2/so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: daampuru <daampuru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 17:43:48 by daampuru          #+#    #+#             */
-/*   Updated: 2023/06/04 17:14:15 by daampuru         ###   ########.fr       */
+/*   Updated: 2023/06/04 17:41:12 by daampuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,18 @@ typedef struct	s_gui {
 	void	*win;
 }			t_gui;
 
+
 /* ---------- Constants ---------- */
-//keycodes
+// keycodes
 # define KEY_ESC 53
 # define KEY_LEFT 123
 # define KEY_RIGHT 124
 # define KEY_DOWN 125
 # define KEY_UP 126
+
+// Assets relative paths
+# define TEST "./assets/test.xpm"
+
 
 /* ---------- Functions ---------- */
 
--- cursus/lvl2/so_long/src/main.c
+++ cursus/lvl2/so_long/src/main.c
@@ -6,7 +6,7 @@
 /*   By: daampuru <daampuru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 17:45:07 by daampuru          #+#    #+#             */
-/*   Updated: 2023/06/04 17:35:24 by daampuru         ###   ########.fr       */
+/*   Updated: 2023/06/04 18:07:16 by daampuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,73 @@ char	close_gui(t_gui	*gui)
 	exit (0);
 }
 
+char	render_img(t_gui *gui, int x, int y)
+{
+	void	*img;
+	int		img_width;
+	int		img_height;
+
+	img = mlx_xpm_file_to_image(gui->mlx, TEST, &img_width, &img_height);
+	if (!img)
+	{
+		ft_putstr_fd("ERROR: Corrupt .xpm file\n", 2);
+		close_gui(gui);
+	}
+	mlx_put_image_to_window(gui->mlx, gui->win, img, x, y);
+	return (0);
+}
+
+char move(int keycode, t_gui *gui)
+{
+	static unsigned int	x;
+	static unsigned int	y;
+
+	if (!x)
+		x = 0;
+	if (!y)
+		y = 0;
+	if (keycode == KEY_LEFT)
+		x -= 10;
+	else if (keycode == KEY_RIGHT)
+		x += 10;
+	else if (keycode == KEY_DOWN)
+		y += 10;
+	else if (keycode == KEY_UP)
+		y -= 10;
+	render_img(gui, x, y);
+	return (1);
+}
+
 int	key_hook(int keycode, t_gui *gui)
 {
+	static unsigned int	moves;
+	int					is_move;
+
+	is_move = 0;
 	if (keycode == KEY_ESC)
 		close_gui(gui);
 	else if (keycode == KEY_LEFT)
-		ft_printf("Left = %i\n", keycode);
+		is_move = move(KEY_LEFT, gui);
 	else if (keycode == KEY_RIGHT)
-		ft_printf("RIGHT = %i\n", keycode);
+		is_move = move(KEY_RIGHT, gui);
 	else if (keycode == KEY_DOWN)
-		ft_printf("DOWN = %i\n", keycode);
+		is_move = move(KEY_DOWN, gui);
 	else if (keycode == KEY_UP)
-		ft_printf("UP = %i\n", keycode);
+		is_move = move(KEY_UP, gui);
 	else
 		ft_printf("No Hooked key = %i\n", keycode);
+	if (!moves)
+		moves = 0;
+	if (is_move)
+		ft_printf("Total movement number %i\n", ++moves);
 	return (keycode);
 }
 
 char	start_gui(t_gui *gui)
 {
-	char	*relative_path = "./assets/test.xpm";
-	int		img_width = 100;
-	int		img_height = 100;
-	void	*img;
-
 	gui->mlx = mlx_init();
-	gui->win = mlx_new_window(gui->mlx, 1920, 1080, "so_long");
-	img = mlx_xpm_file_to_image(gui->mlx, relative_path, &img_width, &img_height);
-	mlx_put_image_to_window(gui->mlx, gui->win, img, 10, 10);
+	gui->win = mlx_new_window(gui->mlx, 1920, 1080, "Bicho Blanco");
+	render_img(gui, 10, 40);
 	mlx_key_hook(gui->win, key_hook, gui);
 	mlx_hook(gui->win, 17, 1L<<0, close_gui, gui);
 	mlx_loop(gui->mlx);
